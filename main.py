35a5d94cc00b3f4a9a5181959e8067e2779d0a6c
SvineruS <37367870+SvineruS@users.noreply.github.com>
2020-11-23 02:05:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def main(url: str) -> bytes:
 
 
 if __name__ == '__main__':
-    print(main("https://www.tiktok.com/@sinyaba/video/6867949158263721221"))
+    vid = main("https://www.tiktok.com/@sinyaba/video/6867949158263721221")
+    with open('dude.mp4', 'wb') as f:
+        f.write(vid)
 
 
